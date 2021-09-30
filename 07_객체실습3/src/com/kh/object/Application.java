f01c4722fd5598dc87d14cdf48df318f41235d7f
Athesana <ssskdsal@gmail.com>
2021-09-30 21:08:50 +0900

    20210930_객체실습4 추가

--- 07_객체실습3/src/com/kh/object/Application.java
+++ 07_객체실습3/src/com/kh/object/Application.java
@@ -1,6 +1,7 @@
 package com.kh.object;
 
 import com.kh.object.practice.NonStaticSample;
+import com.kh.object.practice.StaticSample;
 
 public class Application {
 
@@ -20,6 +21,29 @@ public class Application {
 		System.out.println("3번 : " + sample.alphabette());
 		
 		System.out.println("4번 : " + sample.mySubstring("apple", 2, 4));
+		
+		// StaticSample 구간
+		
+//		ss.toUpper();
+//		<StaticSample 클래스의 필드인 value를 "apple"로 초기화 후 출력하는 역할>
+		StaticSample.setValue("Java");
+		System.out.println("value : " + StaticSample.getValue());
+		
+//		<toUpper 메소드를 실행해서 출력하는 역할>
+		StaticSample.toUpper();
+		System.out.println("대문자 : " + StaticSample.getValue());
+		
+//		<valueLength>
+		System.out.println(StaticSample.valueLength());
+		
+//		<Concat>
+		System.out.println(StaticSample.valueConcat(null));
+//		System.out.println("PROGRAMMING 붙여서 : " + StaticSample.valueConcat("PROGRAMMING"));
+		
+//		<setChar>
+		StaticSample.setChar(0, 'C');
+		System.out.println("J를 C로 바꾸기 : " + StaticSample.getValue());
+		
 		
 		
 		
--- 07_객체실습3/src/com/kh/object/practice/StaticSample.java
+++ 07_객체실습3/src/com/kh/object/practice/StaticSample.java
@@ -1,9 +1,60 @@
 package com.kh.object.practice;
 
 public class StaticSample {
-
-	private String value = "";
+/* 
+ * <접근 제한자의 종류>
+ * 
+ * private = 비공개 
+ * default = 패키지 공개
+ * protected = 상속 공개
+ * public =  완전 공개
+ * 
+ * <내가 푼 것 - 하지만 문제에서 각 메소드에서 출력문은 조냊하지 않고, 테스트 출력은 main 함수에서 value에 접근하여 출력하라고 해서 정상 작동이지만 다르게 생각해야한다.>
+	private static String value = "";
+	
+	public static void toUpper() {
+		String str = new String("Java");
+		System.out.println(str.toUpperCase());
+		
+	}
+*/	
+	private static String value;
+	
+	
+	public static String getValue() {
+		return value;
+	}
+	
+	public static void setValue(String value) {
+		StaticSample.value = value;
+	}
+	
+	public static void toUpper() {
+		value = value.toUpperCase();
+	}
+	
+	
+	public static void setChar(int index, char c) {
+		
+		char[] cArr = value.toCharArray();
+		
+		cArr[index] = c;
+		
+		value = String.valueOf(cArr);
+	
+	}
+	
+	
+	public static int valueLength() {
+		return value.length();
+		
+	}	
+	
+	public static String valueConcat(String str) {
+		return value.concat("PROGRAMMING");
+//		return value + str;
+	}
 	
 	
 	
-}
+}	
