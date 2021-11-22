a871283803333b87bab44158f82751b8177bcf69
Athesana <ssskdsal@gmail.com>
2021-11-22 23:39:48 +0900

    20211122 lambda 복습

--- 16_람다식/src/com/kh/lambda/basic/B_Functional.java
+++ 16_람다식/src/com/kh/lambda/basic/B_Functional.java
@@ -9,9 +9,14 @@ public class B_Functional {
 	private int num = 100;
 	
 	public void method1() {
+//		 () -> {} 이 람다식이 A_FuncInterface 를 구현하는 익명 구현 객체로 만들어져서 변수 fi에 담겨진다.
+//		람다식은 만든다고 해서 바로 실행되는 것이 아니라, A_FuncInterface 인터페이스의 추상 메소드를 구현해주는 것이기 때문에
+//		객체의 메소드를 호출하듯이 실행시켜야 한다.
+		
 		A_FuncInterface fi = () -> {
-//			String str = "매개변수와 반환값이 없어요~~";
 			System.out.println("매개변수와 반환값이 없는 람다식 1");
+//			이것도 나름 코드 블록이기 때문에 안에서 변수 선언, 호출도 가능하다.
+//			String str = "매개변수와 반환값이 없어요~~";
 //			System.out.println(str);
 		};
 		
@@ -35,7 +40,7 @@ public class B_Functional {
 		
 		fi.method();
 		
-//		fi = new A_Functional() {
+//		fi = new A_FuncInterface() {
 //			
 //			@Override
 //			public void method() {
@@ -56,7 +61,8 @@ public class B_Functional {
 		fi.method(5);
 		
 		// 매개변수의 자료형을 생략 가능
-		// 매개변수가 하나이면 괄호() 생략 가능
+		// 매개변수가 하나이면 소괄호() 생략 가능
+		// 실행구문이 하나여서 중괄호{} 생략 가능
 		fi = a -> System.out.println(a * 5);			
 		
 		fi.method(10);
@@ -76,6 +82,7 @@ public class B_Functional {
 		
 		System.out.println("10 + 20 = " + result);
 		
+		// 위 구문을 축약해보자.
 		// 매개변수의 자료형, return 구문과 중괄호{} 생략 가능
 		fi = (x, y) -> x * y;
 		
@@ -84,7 +91,8 @@ public class B_Functional {
 	
 	/*
 	 * 람다식의 실행 블록에서는 필드(인스턴스 변수) 및 로컬변수(매개변수)를 사용할 수 있다.
-	 * 필드(인스턴스 변수)는 제약사항 없이 사용하지만, 로컬변수(매개변수)는 제약사항이 따른다.
+	 * 필드(인스턴스 변수)는 제약사항 없이 사용하지만 (출력, 연산 & 변경 okay), 
+	 * 로컬변수(매개변수)는 제약사항이 따른다 (출력, 연산 okay but 변경 nope!).
 	 * 람다식에서 사용되는 this는 익명 구현 객체의 참조가 아니라 람다식을 실행하는 객체의 참조이다.
 	 * 람다식에서 로컬변수(매개변수)를 이용할 때는 변수들이 final 이거나 effectively final이여야 한다.
 	 * 	- 로컬변수(매개변수)는 람다식에서 읽는 것은 허용하지만, 람다식 내부에서 또는 외부에서 변경할 수 없다.
@@ -93,31 +101,34 @@ public class B_Functional {
 	
 	
 	public void method4(int arg) {
+		//로컬 변수 선언
 		int num = 20;
 		
 // 		필드(인스턴스 변수) 사용
+
+//		field의 값을 변경하는 것 가능
 //		this.num = 200;
 		// 익명 구현 객체 안에서 위에 있는 private int num에 접근하고 싶어요.
 		A_FuncInterface fi = () -> {
+//		람다식 내부에서도 field의 값을 변경하는 것 가능
 //			this.num = 300;
 			
 			System.out.println(this.num);
 		};
 		
 		fi.method(); // this.num => 100이 찍힌다.
-//		람다식에서의 this는 람다식을 실행하는 객체 B_Functional을 의미한다.
-//		field의 값을 변경하는 것 가능
+//		람다식에서의 this는 람다식을 실행하는 객체를( B_Functional )을 의미한다.
 		
 //		fi = new A_FuncInterface() {
 //			
 //			@Override
 //			public void method() {
-//				System.out.println(this.num);
-//			// 익명 구현 객체의 this는 		
+//				System.out.println(this.num); // this.num은 컴파일 에러 발생
+//			// 익명 구현 객체의 this는 실제로 만들어진 익명 구현 객체를 가르킨다. 그런데 이 코드블록 안에서는(이 객체 안) num이라는 변수가 없어서 에러나는 것
 //			}
 //		};
 		
-//		로컬 변수 사용
+//		로컬 변수 | 매개변수 사용
 		//Local variable num defined in an enclosing scope must be final or effectively final 에러 발생
 //		num = 200;
 //		arg = 200;
